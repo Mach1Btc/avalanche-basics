e3f10780dd167e47cd9da4ff657b9187b47c1101
Mach1Btc <allxcaps@gmail.com>
2025-06-08 19:03:42 -0500

    update gitignore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -42,6 +42,15 @@ const config: HardhatUserConfig = {
         },
       },
       {
+        version: "0.7.5",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
         version: "0.7.6",
         settings: {
           optimizer: {
